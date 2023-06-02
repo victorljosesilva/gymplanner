c32cbe76eccee87f9564ed601a1b48c180912ce7
Victor Leonardo José da Silva <e054820@gmail.com>
2023-06-01 21:54:41 -0300

    update

--- php/montar_cronograma.php
+++ php/montar_cronograma.php
@@ -34,10 +34,7 @@
                 <th id="cabeca">Grupo Muscular</th>
                 <th id="cabeca">Exercício</th>
                 <th id="cabeca">Serie x Repetição</th>
-                <th id="cabeca">
-                    Duração
-                    (Intervalo)
-                </th>
+                <th id="cabeca">Intervalo/pausa</th>
             </tr>
             <tr>
                 <td></td>
@@ -49,45 +46,138 @@
     </div>
     <main>
         <h3>Escolha Grupo Muscular:</h3>
-        <form action="cadastro_exercicio.php" method="GET">
+        <form>
             <?php
-            // Conexão com o banco de dados (substitua os valores pelos seus próprios)
             $servername = "localhost";
             $username = "root";
             $password = "#userVL2023";
             $dbname = "gymplanner";
 
-            // Criar conexão
             $conn = new mysqli($servername, $username, $password, $dbname);
 
-            // Verificar conexão
             if ($conn->connect_error) {
                 die("Falha na conexão: " . $conn->connect_error);
             }
 
-            // Consulta SQL para buscar as categorias únicas
+            $categoria = isset($_GET['categoria']) ? $_GET['categoria'] : '';
+
             $sql = "SELECT DISTINCT categoria FROM exercicios";
             $result = $conn->query($sql);
 
-            // Verificar se há resultados
             if ($result->num_rows > 0) {
-                // Gerar as opções do select com base nas categorias
-                echo '<select name="categoria">';
-                echo '<option value="">Selecione</option>';
+                echo '<select id="categoria" name="categoria">';
+                echo '<option value="">Selecione uma categoria</option>';
                 while ($row = $result->fetch_assoc()) {
-                    echo '<option value="' . $row['categoria'] . '">' . $row['categoria'] . '</option>';
+                    $selected = $categoria === $row['categoria'] ? 'selected' : '';
+                    echo '<option value="' . $row['categoria'] . '" ' . $selected . '>' . $row['categoria'] . '</option>';
                 }
                 echo '</select>';
-                echo '<button type="submit">Selecionar</button>';
             } else {
                 echo 'Nenhuma categoria encontrada.';
             }
 
-            // Fechar conexão com o banco de dados
+            $exercicios = array();
+            if (!empty($categoria)) {
+                $sql = "SELECT * FROM exercicios WHERE categoria = ?";
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param("s", $categoria);
+                $stmt->execute();
+                $result = $stmt->get_result();
+
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        $exercicio = array(
+                            'id' => $row['id'],
+                            'nome' => $row['nome'],
+                            'series' => $row['series'],
+                            'repeticao' => $row['repeticao'],
+                            'tempo' => $row['tempo']
+                        );
+                        array_push($exercicios, $exercicio);
+                    }
+                }
+                $stmt->close();
+            }
+
             $conn->close();
             ?>
+            <br>
+            <h3>Escolha o Exercício:</h3>
+            <select id="exercicio" name="exercicio">
+                <?php
+                foreach ($exercicios as $exercicio) {
+                    echo '<option value="' . $exercicio['id'] . '">' . $exercicio['nome'] . '</option>';
+                }
+                ?>
+            </select>
+
+            <br>
+            <h3>Escolha a Série:</h3>
+            <select id="serie" name="serie">
+                <?php
+                foreach ($exercicios as $exercicio) {
+                    echo '<option value="' . $exercicio['series'] . '">' . $exercicio['series'] . '</option>';
+                }
+                ?>
+            </select>
+
+            <br>
+            <h3>Escolha a Repetição:</h3>
+            <select id="repeticao" name="repeticao">
+                <?php
+                foreach ($exercicios as $exercicio) {
+                    echo '<input value="' . $exercicio['repeticao'] . '">' . $exercicio['repeticao'] . '</input>';
+                }
+                ?>
+            </select>
+
+            <br>
+            <h3>Escolha o Tempo:</h3>
+            <select id="tempo" name="tempo">
+                <?php
+                foreach ($exercicios as $exercicio) {
+                    echo '<option value="' . $exercicio['tempo'] . '">' . $exercicio['tempo'] . '</option>';
+                }
+                ?>
+            </select>
+
+            <br><br>
+            <button type="button" onclick="adicionarExercicio()">Adicionar Exercício</button>
         </form>
     </main>
+
+    <script>
+        // Função para adicionar o exercício selecionado à tabela
+        function adicionarExercicio() {
+            var exercicioSelect = document.getElementById('exercicio');
+            var exercicioSelecionado = exercicioSelect.options[exercicioSelect.selectedIndex].text;
+
+            var tabela = document.querySelector('.tabela table');
+            var novaLinha = tabela.insertRow();
+
+            var colunaGrupoMuscular = novaLinha.insertCell();
+            var colunaExercicio = novaLinha.insertCell();
+            var colunaSerieRepeticao = novaLinha.insertCell();
+            var colunaDuracao = novaLinha.insertCell();
+
+            colunaGrupoMuscular.textContent = document.getElementById('categoria').value;
+            colunaExercicio.textContent = exercicioSelecionado;
+            colunaSerieRepeticao.textContent = document.getElementById('serie').value + ' x ' + document.getElementById('repeticao').value;
+            colunaDuracao.textContent = document.getElementById('tempo').value + ' min';
+
+            // Limpa os campos de seleção
+            exercicioSelect.selectedIndex = 0;
+            document.getElementById('serie').selectedIndex = 0;
+            document.getElementById('repeticao').selectedIndex = 0;
+            document.getElementById('tempo').selectedIndex = 0;
+        }
+
+        // Evento de alteração na seleção de categoria
+        document.getElementById('categoria').addEventListener('change', function() {
+            var categoria = document.getElementById('categoria').value; // Obtém a categoria selecionada
+            location.href = 'montar_cronograma.php?categoria=' + encodeURIComponent(categoria);
+        });
+    </script>
 </body>
 
 </html>
